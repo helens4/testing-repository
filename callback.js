ea2237b4e353ed1c8508a00cc2a21373342751a5
helens <80rass@gmail.com>
2020-09-14 09:24:53 +0200

    first try

---
+++ callback.js
@@ -0,0 +1,31 @@
+const posts = [
+    { title: 'post one', body: 'this is post one' },
+    { title: 'post two', body: 'this is post two' }
+]
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+
+        posts.forEach(post => {
+            output += `<li> ${post.title} ${post.body} </li>`
+        })
+
+        document.body.innerHTML = `<ul> ${output} </ul>`;
+    }, 1000);
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post)
+        callback()
+    }, 2000)
+}
+
+
+//getPosts()
+
+createPost({ title: 'post three', body: 'this is post three' }, getPosts)
+
+
+
---
+++ promises.js
@@ -0,0 +1,22 @@
+const colorTypeA = document.querySelectorAll("input")[0]
+const colorTypeB = document.querySelectorAll("input")[1]
+
+let colorLeft = null, colorRight = null;
+
+colorTypeA.addEventListener("input", function (event) {
+    colorLeft = event.target.value
+    console.log('color left', colorLeft)
+    if (colorRight !== null)
+        setGradient();
+})
+
+colorTypeB.addEventListener("input", function (event) {
+    colorRight = event.target.value
+    console.log('color right', colorRight)
+    if (colorLeft !== null)
+        setGradient()
+})
+
+const setGradient = () => {
+    document.body.style.background = `linear-gradient(to left, ${colorLeft}, ${colorRight})`
+}
